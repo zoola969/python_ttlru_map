dd07e5a7f2890f07ece2f95d05b77b89716b32a4
Alexandr Dmitriev <alexandr.dmitriev@myjet.local>
2024-04-14 14:10:22 +0300

    fix linters

--- tests/test_setitem.py
+++ tests/test_setitem.py
@@ -12,12 +12,14 @@ def test_setitem__new_item():
     key = 1
     value = 1
     expected_node = DoubleLinkedListNode(value=_LinkedListValue(time_=time_, key=key))
-    with patch.object(TTLDict, "_pop_ll_node") as mock_pop_ll_node:
-        with patch.object(TTLDict, "_put_node_to_end") as mock_put_node_to_end:
-            d._setitem(key, value, time_)
-            mock_pop_ll_node.assert_not_called()
-            mock_put_node_to_end.assert_called_once()
-            assert mock_put_node_to_end.call_args[0][0] == expected_node
+    with patch.object(TTLDict, "_pop_ll_node") as mock_pop_ll_node, patch.object(
+        TTLDict,
+        "_put_node_to_end",
+    ) as mock_put_node_to_end:
+        d._setitem(key, value, time_)
+        mock_pop_ll_node.assert_not_called()
+        mock_put_node_to_end.assert_called_once()
+        assert mock_put_node_to_end.call_args[0][0] == expected_node
     assert d._dict == {key: _DictValue(value=value, node=expected_node)}
 
 
@@ -29,10 +31,12 @@ def test_setitem__existing_item():
     d[key] = value
     old_node = d._dict[key].node
     expected_node = DoubleLinkedListNode(value=_LinkedListValue(time_=time_, key=key))
-    with patch.object(TTLDict, "_pop_ll_node") as mock_pop_ll_node:
-        with patch.object(TTLDict, "_put_node_to_end") as mock_put_node_to_end:
-            d._setitem(key, value, time_)
-            mock_pop_ll_node.assert_called_once_with(old_node)
-            mock_put_node_to_end.assert_called_once()
-            assert mock_put_node_to_end.call_args[0][0] == expected_node
+    with patch.object(TTLDict, "_pop_ll_node") as mock_pop_ll_node, patch.object(
+        TTLDict,
+        "_put_node_to_end",
+    ) as mock_put_node_to_end:
+        d._setitem(key, value, time_)
+        mock_pop_ll_node.assert_called_once_with(old_node)
+        mock_put_node_to_end.assert_called_once()
+        assert mock_put_node_to_end.call_args[0][0] == expected_node
     assert d._dict == {key: _DictValue(value=value, node=expected_node)}
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,3 @@
 from unittest.mock import Mock
 
-LockMock = lambda: Mock(__enter__=Mock(), __exit__=Mock())
+LockMock = lambda: Mock(__enter__=Mock(), __exit__=Mock())  # noqa: E731
