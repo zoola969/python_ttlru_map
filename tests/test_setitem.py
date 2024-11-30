bd023a402d3eece6b8a6e9d5302d60df7dca39ff
Alexander Dmtriev <zoola969@gmail.com>
2024-11-30 15:24:00 +0400

    Drop 3.8 support

--- tests/test_setitem.py
+++ tests/test_setitem.py
@@ -12,10 +12,13 @@ def test_setitem__new_item():
     key = 1
     value = 1
     expected_node = DoubleLinkedListNode(value=_LinkedListValue(time_=time_, key=key))
-    with patch.object(TTLMap, "_pop_ll_node") as mock_pop_ll_node, patch.object(
-        TTLMap,
-        "_put_node_to_end",
-    ) as mock_put_node_to_end:
+    with (
+        patch.object(TTLMap, "_pop_ll_node") as mock_pop_ll_node,
+        patch.object(
+            TTLMap,
+            "_put_node_to_end",
+        ) as mock_put_node_to_end,
+    ):
         d._setitem(key, value, time_)
         mock_pop_ll_node.assert_not_called()
         mock_put_node_to_end.assert_called_once()
@@ -31,10 +34,13 @@ def test_setitem__existing_item():
     d[key] = value
     old_node = d._dict[key].node
     expected_node = DoubleLinkedListNode(value=_LinkedListValue(time_=time_, key=key))
-    with patch.object(TTLMap, "_pop_ll_node") as mock_pop_ll_node, patch.object(
-        TTLMap,
-        "_put_node_to_end",
-    ) as mock_put_node_to_end:
+    with (
+        patch.object(TTLMap, "_pop_ll_node") as mock_pop_ll_node,
+        patch.object(
+            TTLMap,
+            "_put_node_to_end",
+        ) as mock_put_node_to_end,
+    ):
         d._setitem(key, value, time_)
         mock_pop_ll_node.assert_called_once_with(old_node)
         mock_put_node_to_end.assert_called_once()
