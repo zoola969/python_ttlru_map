4f8e5db72605b7b04dd849d3ab88cac39ad91992
Alexander Dmtriev <zoola969@gmail.com>
2025-09-05 20:39:49 +0400

    Update versioning and improve test commands in CI configuration

--- ttlru_map/__init__.py
+++ ttlru_map/__init__.py
@@ -1,7 +1,9 @@
+from importlib.metadata import version
+
 from ._exceptions import TTLMapError, TTLMapInvalidConfigError
 from ._ttl_map import TTLMap
 
-__version__ = "1.1.0"
+__version__ = version("ttlru_map")
 
 __all__ = [
     "TTLMap",
